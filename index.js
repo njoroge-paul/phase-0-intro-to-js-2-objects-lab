b1ad525ac48e12a506f35fd790b35b0e6d82863e
Duncan <njorogedun870@gmail.com>
2024-07-08 10:36:51 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: 'John Doe',
+    age: 30,
+    city: 'New York'
+    
+}
+function updateEmployeeWithKeyAndValue(employee, occupation , Engineer){
+    const newEmployee = { ...employee};    
+    newEmployee[occupation] = Engineer;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, occupation, writer){
+    employee[occupation] = writer;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, hobby){
+    const newEmployee = {...employee};
+    delete newEmployee[hobby];
+    return newEmployee;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, hobby){
+    delete employee[hobby];
+    return employee;
+}
